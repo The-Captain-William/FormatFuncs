933f63ab40d0e31f72d5d5b26a8dcd4c5cff65d5
William Rivera <william.r.2048@gmail.com>
2022-12-22 17:21:34 -0500

    resolved tail index and slice

--- src/FormatFuncs/formatfuncs.py
+++ src/FormatFuncs/formatfuncs.py
@@ -14,6 +14,7 @@ def string_format(string, length=50, paragraph_size=0,space_size=1):
     but will try to approximate as best as possible.
     """
     string_length = len(string)
+    print(f"string length: {string_length}")
     ratio = string_length // length
     index_jump = floor(0.8 * length) 
     composite_string = ''
@@ -23,6 +24,9 @@ def string_format(string, length=50, paragraph_size=0,space_size=1):
     DEFINITE_END = string_length - 1
     rotate = True
     space = False
+
+
+
     while rotate is True:
         for turn, rotation in enumerate(range(ratio)):
             index_start = index_end_non_inclusive
@@ -37,11 +41,21 @@ def string_format(string, length=50, paragraph_size=0,space_size=1):
                     space = True
             except ZeroDivisionError:
                 pass
-            if space == True and "." in nth_snippet[0:-1]:  # if its time for a space
+            
+            # PARAGRAPH MODE ON 
+                #check and see if paragraph mode is on and sentance is invalid
+
+            if space == True and "." in nth_snippet[0:-1]:  # if its time for a space, and there is a period inside the middle of the snippet
+                index_zero = nth_snippet[0]  # current nth_snippet character
+                index_zero_value = 0 # index value
                 hold_this = ''
                 period_index = nth_snippet.find(".")
-                hold_this = nth_snippet[1:period_index + 1] + ("\n" * 2) + nth_snippet[period_index + 2:]
-                #print(f"Hold:#{hold_this}#")
+                if index_zero != " " or "\n":  # include char
+                    index_zero_value = 0
+                else:
+                    index_zero_value = 1  # skip char
+                hold_this = nth_snippet[index_zero_value:period_index + 1] + ("\n" * 2) + nth_snippet[period_index + 2:]
+                print(f"Hold:#{hold_this}#")
                 nth_snippet = hold_this
                 nth_snippet += ("\n") * space_size
                 nth_snippet.lstrip()
@@ -51,33 +65,55 @@ def string_format(string, length=50, paragraph_size=0,space_size=1):
             else:
                 nth_snippet += "\n"
                 composite_string += nth_snippet.lstrip()
+        
+
+
+
 
         # CONDITIONS FOR TAIL END #        
         nth_snippet = string[index_end_non_inclusive:DEFINITE_END + 1]  # end of string established
-
-        length_of_final = len(nth_snippet) // 2
-        while nth_snippet[length_of_final] != " ":
-            length_of_final +=1
-        if nth_snippet[length_of_final] == " ":
-            start = 0
-            if start == "\n" or " ":
-                start = 1
-            else:
-                start = 0
-            hold_this = nth_snippet[start:length_of_final + 1] + "\n" + nth_snippet[length_of_final + 1:]  # split the last nth and add a breakline
-            #print(f" hold final #{hold_this}#")
+        start = 0
+        lenth_of_tail_end = len(nth_snippet)
+        if lenth_of_tail_end <= length:  # length + length * .2 ? 
+            pass
+        else:
+            left_over = length - lenth_of_tail_end
+            while nth_snippet[left_over] != " ":
+                try: 
+                    left_over -= 1
+                except IndexError:
+                    left_over += 1
+            if nth_snippet[left_over] == " ":
+                hold_this = nth_snippet[start:left_over + 1] + "\n" + nth_snippet[left_over + 1:]  # split the last nth and add a breakline
             nth_snippet = hold_this
-            composite_string += nth_snippet.lstrip()
+            #print(f" hold final #{hold_this}#")
+            #nth_snippet = hold_this
+
         #remainder = (DEFINITE_END - index_end_non_inclusive) 
 
         #print(f"nth snippet: {nth_snippet} index end: {index_end_non_inclusive} str length: {string_length}  def end: {DEFINITE_END}") # debug
+        composite_string += nth_snippet.lstrip()
         rotate = False
         return composite_string
     # TODO
     # the function has to account for spaces
+    # FIX the algorithm that divides the last string into two:
+    # check to see if the length of the tail is less than
+    # or equal to the specified parameter length. 
+    # if so, just leave it alone. 
+    # if it is get the length total, attempt to chop off another remainder
+    # slap the remaining on, then slap on the remainder.  
 
 
 
+#print(string_format("Listen to rich people. It's that simple.", length=50))
+#print(string_format("Raw action solves everything. Caution breeds fear.", length=40, paragraph_size=0))
+
+
+#print(string_format("Listen to rich people. It's that simple.", length=50))
+
+
+print(string_format("If you're visiting this page, you're likely here because you're searching for a random sentence. Sometimes a random word just isn't enough, and that is where the random sentence generator comes into play. By inputting the desired number, you can make a list of as many random sentences as you want or need. Producing random sentences can be helpful in a number of different ways.", paragraph_size=2, length=40))
 
 def paragraph_maker(string, paragraph_size=3, space_size=1):
     """
@@ -116,5 +152,3 @@ def paragraph_maker(string, paragraph_size=3, space_size=1):
 
 
 
-#print(string_format("Listen to rich people. It's that simple.", length=50))
-print(string_format("Raw action solves everything. Caution breeds fear.", paragraph_size=1))
